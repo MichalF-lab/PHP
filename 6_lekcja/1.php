771972142d60fef37a2bb8c72568de874fded1ad
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-10-10 20:19:14 +0200

    commit to master

---
+++ 6_lekcja/1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head> 
+        <meta charset="utf-8">
+        <title>Wiek ziomeczków</title>
+    </head>
+    <body>
+    <?php
+    if(!is)
+    echo <<< COKOLWIEK
+        <form method="POST">
+            <input type="number" name="ile_typa" placeholder="ile typa wariacie?"><br><br>
+            <input type="submit" name="git"><br><br>
+        </form>
+    COKOLWIEK;
+
+    echo <<< new
+        <form method="POST">
+            <input type="number" name="wiek_typa" placeholder="ile typ ma lat?"><br><br>
+            <input type="submit" name="wiek"><br><br>
+        </form>
+    new;
+
+    ?>
+    </body>
+</html>
