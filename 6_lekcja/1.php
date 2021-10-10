12130dad1ae9ac0cf2274299e01fc3248482018c
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-10-11 00:07:40 +0200

    commit to master

--- 6_lekcja/1.php
+++ 6_lekcja/1.php
@@ -6,20 +6,62 @@
     </head>
     <body>
     <?php
-    if(!is)
-    echo <<< COKOLWIEK
-        <form method="POST">
-            <input type="number" name="ile_typa" placeholder="ile typa wariacie?"><br><br>
-            <input type="submit" name="git"><br><br>
-        </form>
-    COKOLWIEK;
+        if(!isset($_POST['ile_typa']))
+        {
+        echo <<< COKOLWIEK
+            <form method="POST">
+                <input type="number" name="ile_typa" placeholder="ile typa wariacie?"><br><br>
+                <input type="submit" name="git"><br><br>
+            </form>
+        COKOLWIEK;
+        }
+        if(isset($_POST['ile_typa']))
+        {
+           
+            echo <<< new
+                <form method="POST">
+            new;
+            for($x = 0; $x < $_POST['ile_typa']; $x++)
+                {
+                echo <<< new
+                    <input type="number" name="wiek_typa$x" placeholder="ile typ ma lat?"><br><br>
+                new;
+                }
+                echo <<< new
+                    <input type="submit" name="wiek"><br><br>
+                    </form>
+                new;
+        }
+        if(isset($_POST['wiek']))
+        {
+            $suma = 0;
+            $srednia = 0;
+            // for($x = 0; $x < $_POST['ile_typa']; $x++)
+            // {
+            //     $suma += $_POST["wiek_typa'$x'"];
+            // }
 
-    echo <<< new
-        <form method="POST">
-            <input type="number" name="wiek_typa" placeholder="ile typ ma lat?"><br><br>
-            <input type="submit" name="wiek"><br><br>
-        </form>
-    new;
+            // foreach($_POST as $key => $value)
+            // {
+            //     $suma+= $value;
+            // }
+            // $srednia = $suma / $_POST['ile_typa'];
+            $count = 0;
+            //echo $_POST['wiek'];
+            //print_r($_POST);
+            foreach ($_POST as $key => $value)
+            {
+                if ($key != 'wiek')
+                {
+                    $suma+=$value;
+                    $count++;
+                }
+            }
+            $srednia = $suma / $count;
+            echo "<br><br>";
+            echo $srednia;
+            
+        }
 
     ?>
     </body>
