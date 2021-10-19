6debd00dc5da804e2d518350ccc3127ad2636f0d
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-10-19 07:53:40 +0200

    Add files via upload

--- 6.php
+++ 6.php
@@ -1,52 +1,47 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            if(!isset($_POST['person']))
-            {
-                echo <<< FORM
-                <h3>ilosc osób w rodzinie</h3>
-                <form method="POST">
-                    <input type='number' name='person' placeholder='podaj ilość osób'><br><br>
-                    <input type="submit">
-                </form>
-                FORM;
-            }
-        ?>
-        <?php
-            if(!empty($_POST['person']))
-            {
-                echo <<< FORM
-                <h3>ilosc osób w rodzinie</h3>
-                <form method="POST">
-                FORM;
-                    $count = $_POST['person'];
-                    for($i=0; $i <$count; $i++)
-                    {
-                        echo "<input type='number' name='person$i' placeholder='podaj wiek'><br><br>";
-                    }
-                echo <<< FORM
-                    
-                <input type="submit" name='buttonAVG'>
-                </form>
-    
-                FORM;
-            }
-            if(!isset($_POST['buttonAVG']))
-            {
-                $avg=0;
-                $count =0;
-                foreach ($_POST as $key => $value)
-                {
-                    if($avg != 'buttonAVG')
-                    {
-                    $avg += $value;
-                    $count++;
-                    }
-                }
-                echo "Średni wiek" .$avg/$count ."<br>";
-            }
+<meta charest="utf-8">
+<?php
+    echo "dzien-miesiac-rok".date('d-m-Y').'r.<br>';
+    echo date('m-d-y').'r.,<br>';
+    echo date('d-M-y').'r.<br>';
+    echo date('d-F-Y').'r.<br>';
 
-        ?>
-    </body>
-</html>
+    setlocale(LC_ALL, 'PL');
+    echo strftime('%d %B %Y').'<br>';
+
+    echo date('G:i:s').'<br>';
+?>
+
+<script>
+    setTimeout(() => {
+        //window.location.reload();
+    }, 1000);
+</script>
+
+<?php
+    echo date('H:i:s').'<br>'; // na kartkówke
+    echo date('H:i:sa').'<br>';
+
+    $data = getdate();
+
+    print_r($data);
+    echo '<br>';
+
+    // rok przestęny
+
+    echo date("L");
+    // 0  - rok nie jest przestępny
+
+    // mktime
+
+    $today = mktime(0,0,0, date("m"), date("d"), date("Y"));
+
+    echo $today.'<br>';
+    // lata od 1 stycznia 1970
+    $x=$today/(60*60*24*365);
+
+    echo (int)$x.'<br>';
+
+    $last  = mktime(0,0,0, date("m"), date("d"), date("Y") -1);
+    $x=$last/(60*60*24*365);
+    echo (int)$x.'<br>';
+?>
